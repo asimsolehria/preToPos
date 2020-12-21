4cd95f34e0ad0b3ca3ed3d50c8e6d9e38fea778d
Asim Habib <de.asimhabib@gmail.com>
2020-12-21 23:49:27 +0500

    wrote a method to covert prefix expression to postfix expression using stack

--- src/pretopos/PreToPos.java
+++ src/pretopos/PreToPos.java
@@ -1,6 +1,8 @@
 
 package pretopos;
 
+import java.util.Stack;
+
 /**
  *
  * @author asim
@@ -8,4 +10,25 @@ package pretopos;
 public class PreToPos
 {
     
+    public String preToPos(String exp)
+    {
+        Stack<String> stack= new Stack<>();
+        
+        for (int i = exp.length()-1; i >=0; i--) 
+        {
+            char c=exp.charAt(i);
+            
+            if(Character.isLetterOrDigit(c))
+                stack.push(c+"");
+            else
+            {
+                String opnd1=stack.pop();
+                String opnd2=stack.pop();
+                String postFix=opnd1+opnd2+c;
+                stack.push(postFix);
+            }
+            
+        }
+        return stack.pop();
+    }
 }
---
+++ src/pretopos/main.java
@@ -0,0 +1,16 @@
+
+package pretopos;
+
+/**
+ *
+ * @author asim
+ */
+public class main 
+{
+    public static void main(String[] args) 
+    {
+        String preFix="*-A/BC-/AKL";
+        
+        System.out.println(new PreToPos().preToPos(preFix));
+    }
+}
